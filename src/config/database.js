f53e0bc11843d73d094aee276aab9270e2eca961
RoberioAlves <roberiouol@gmail.com>
2024-05-15 16:37:20 -0300

    Atualizaçoes do projeto.

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhoste',
+    port: 5432,
+    username: 'postgres',
+    password: 'postegres',
+    database: 'DEVBUGER',
+    define: {
+        timestamps:  true,
+        underscore: true,
+        underscoreAll: true,
+    }
+}
